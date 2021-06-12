a1679d616806934f1f5fc28a4a1af3e7ee219394
Amir Sharapov <amirfounder18@gmail.com>
2021-06-12 13:21:43 -0500

    fixed warnings for deployement

--- src/components/pages/blog/Blog.jsx
+++ src/components/pages/blog/Blog.jsx
@@ -1,11 +1,11 @@
 import React from 'react'
-import styles from './Blog.module.scss'
+// import styles from './Blog.module.scss'
 import PageWrapper from '../../wrappers/page/PageWrapper'
 
 const Blog = () => {
   return(
     <PageWrapper>
-      Blog.
+      Blog
     </PageWrapper>
   )
 }
--- src/components/pages/projects/Projects.jsx
+++ src/components/pages/projects/Projects.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import styles from './Projects.module.scss'
+// import styles from './Projects.module.scss'
 import PageWrapper from '../../wrappers/page/PageWrapper';
 
 
